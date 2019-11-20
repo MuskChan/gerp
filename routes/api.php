e4a0e11f6964d564fcbdaa9dbd46d0ec11c659a5
MuskChan <523919565@qq.com>
2019-11-20 12:17:06 +0800

    Send SMS verification code

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,10 @@ use Illuminate\Http\Request;
 //});
 
 
-Route::prefix('v1')->name('api.v1.')->group(function (){
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function (){
+    //短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
 
 
