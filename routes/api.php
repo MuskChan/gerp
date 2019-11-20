eb884211cc92e58c5bb88674f27d562577a07480
MuskChan <523919565@qq.com>
2019-11-20 15:47:33 +0800

    frequency limit

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,24 @@ use Illuminate\Http\Request;
 //});
 
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function (){
-    //短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    //用户注册
-    Route::post('/users', 'UsersController@store')
-        ->name('users.store');
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function (){
+        Route::middleware('throttle:'.config('api.rate_limits.access'))
+            ->group(function (){
+            //短信验证码
+            Route::post('verificationCodes', 'VerificationCodesController@store')
+                ->name('verificationCodes.store');
+            //用户注册
+            Route::post('/users', 'UsersController@store')
+                ->name('users.store');
+        });
+
+        Route::middleware('thorttle:'.config('api.rate_limits.sign'))
+            ->group(function () {
+
+            });
 });
 
 
