c804a038a9e94e6e894e730094a59fe604770d6c
MuskChan <523919565@qq.com>
2019-11-24 16:14:08 +0800

    rapid generation JWT

---
+++ app/Console/Commands/GenerateToken.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class GenerateToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gerp:generate-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '快速为用户生成 token';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userId = $this->ask('输入用户 ID');
+
+        $user = User::find($userId);
+
+        if (!$user) {
+            return $this->error('用户不存在');
+        }
+
+        // 一年以后过期
+        $ttl = 365*24*60;
+        $this->info(auth('api')->setTTL($ttl)->login($user));
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
     use Traits\LastActivedAtHelper;
