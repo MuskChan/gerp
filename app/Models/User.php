70e2eb12c2bdbc4cce2ecd40d2493da6cf58729f
MuskChan <523919565@qq.com>
2019-11-24 17:16:54 +0800

    user info

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'weixin_unionid', 'weixin_openid',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::prefix('v1')
     ->namespace('Api')
     ->name('api.v1.')
     ->group(function (){
+        //访问频率限制
         Route::middleware('throttle:'.config('api.rate_limits.access'))
             ->group(function (){
             //图片验证码
@@ -46,8 +47,19 @@ Route::prefix('v1')
             // 删除token
             Route::delete('authorizations/current', 'AuthorizationsController@destroy')
                 ->name('authorizations.destroy');
+            // 某个用户的详情
+            Route::get('users/{user}', 'UsersController@show')
+                ->name('users.show');
+
+            // 登录后可以访问的接口
+            Route::middleware('auth:api')->group(function () {
+                // 当前登录用户信息
+                Route::get('user', 'UsersController@me')
+                    ->name('user.show');
+            });
         });
 
+        //登录相关
         Route::middleware('thorttle:'.config('api.rate_limits.sign'))
             ->group(function () {
 
